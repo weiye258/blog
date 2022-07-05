333990ec6a2bf81b6d2b3a6cbcf9f60ef3b29fa9
weiye258 <1606871689@qq.com>
2022-07-05 20:14:07 +0800

    配置redis和日志

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.http import HttpResponse
+import logging
+logger=logging.getLogger('django')
+def log(request):
+    # 3.使用日志器记录信息
+    logger.info('info')
+    return HttpResponse('test')
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
